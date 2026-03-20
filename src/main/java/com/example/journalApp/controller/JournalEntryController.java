ef3ed4437c922c58582c84a4feca02affd44eece
Aryan Kumar <aryankumar4848@outlook.com>
2026-03-20 16:04:35 +0530

    CRUD and health check APIs done

---
+++ src/main/java/com/example/journalApp/controller/JournalEntryController.java
@@ -0,0 +1,33 @@
+package com.example.journalApp.controller;
+
+import com.example.journalApp.entity.JournalEntry;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/journals")
+public class JournalEntryController {
+
+    private Map<Long, JournalEntry> journalEntries = new HashMap<>();
+
+    @GetMapping
+    public List<JournalEntry> getAll() {
+        return new ArrayList<>(journalEntries.values());
+    }
+
+    @GetMapping("/id/{myId}")
+    public JournalEntry getEntryById(@PathVariable Long myId)
+    {
+        return journalEntries.get(myId);
+    }
+
+    @PostMapping
+    public boolean createEntry(@RequestBody JournalEntry myEntry) {
+        journalEntries.put(myEntry.getId(), myEntry);
+        return true;
+    }
+}
